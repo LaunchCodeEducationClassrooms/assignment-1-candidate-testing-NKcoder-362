ebde7852cb9eeec96b16169d98a9cd92c4f3bc53
Nhlly3262 <nanholly@hotmail.com>
2021-02-22 00:16:25 +0000

    Contains final screen telling candidate if they passed or failed and what their overall scroe is.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -19,21 +19,50 @@ function askForName() {
 candidateName = input.question("Enter Candidate Name: ");
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
+//candidateAnswer = input.question(question)//
+
+for (let i = 0; i<questions.length; i++) {
+  candidateAnswers.push(input.question(questions[i]));
+}
+numCorrectAnswers = 0
+numQuizQuestions = 0
+for (let i = 0; i < correctAnswers.length; i++){
+    if(correctAnswers[i]===candidateAnswers[i]){
+      numCorrectAnswers = numCorrectAnswers + 1;
+    }
+    numQuizQuestions = numQuizQuestions + 1
+}
+//console.log(numCorrectAnswers, numQuizQuestions)//
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(correctAnswer.toLowerCase === candidateAnswer.toLowerCase) {
+/*if(correctAnswer.toLowerCase === candidateAnswer.toLowerCase) {
   console.log("Your answer to the question is correct.")
 } else {
   console.log("Your answer to the question is incorrect.")
- }
+}*/
+console.log ("Candidate Name: ", candidateName);
+let lnNumber = 1;
+for(let i = 0; i < questions.length; i++ ){
+  console.log(lnNumber+") ", questions[i]);
+  console.log("Your answer: ", candidateAnswers[i])
+  console.log("Correct answer: ", correctAnswers[i])
+  lnNumber = lnNumber + 1
+}
+let calGrade = (numCorrectAnswers/numQuizQuestions)*100
 
-  let grade;
-  
+console.log(">>> Overall Grade: ", calGrade+"%" , "("+ numCorrectAnswers, "of", numQuizQuestions, "responses correct) <<<")
+if(calGrade >= 80){
+  console.log(">>> Status: PASSED <<<");
+}else{
+  console.log(">>> Status: FAILED <<<");
+}
+
+
+  let grade = (numCorrectAnswers/numQuizQuestions)*100;
 
   return grade;
 }
@@ -44,9 +73,11 @@ function runProgram() {
   
   console.log("Welcome " + candidateName + " to the Astronaut Training Program.")
 
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
